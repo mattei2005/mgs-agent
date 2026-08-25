1413cbaeac9a14c41bed68fe4a3dc1a9e88a8210
Rodolfo Mattei <mattei2005@gmail.com>
2026-08-25 18:11:57 -0400

    auto: plugins/mgs-direct-quiz/mgs-direct-quiz.php plugins/mgs-direct-quiz/assets/admin.css plugins/mgs-dir

--- plugins/mgs-direct-quiz/mgs-direct-quiz.php
+++ plugins/mgs-direct-quiz/mgs-direct-quiz.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: MGS Direct Quiz
  * Description: Cria e duplica landing pages simples de quiz para tráfego direto, com modelos LP1 e LP2.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: MGS Digital Corp
  */
 
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'MGS_DQ_VERSION', '1.0.2' );
+define( 'MGS_DQ_VERSION', '1.0.3' );
 define( 'MGS_DQ_FILE', __FILE__ );
 define( 'MGS_DQ_PATH', plugin_dir_path( __FILE__ ) );
 define( 'MGS_DQ_URL', plugin_dir_url( __FILE__ ) );
